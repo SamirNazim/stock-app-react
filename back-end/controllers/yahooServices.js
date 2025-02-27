59ccd0f75b144a5e91fbf74e85aa3d5fd0ff8c83
Samir Nazim <samirn@Live.ca>
2025-02-27 13:24:19 -0500

    upgrade yahoofinance2 library

--- back-end/controllers/yahooServices.js
+++ back-end/controllers/yahooServices.js
@@ -1,66 +1,51 @@
-const yahooFinance = require("yahoo-finance");
+// require syntax (if your code base does not support imports)
+const yahooFinance = require("yahoo-finance2").default; // NOTE the .default
 
-exports.getTickerFinancialData = (req, res) => {
-  return new Promise(function (resolve, reject) {
-    yahooFinance.quote(
-      {
-        symbol: req.params.symbol,
-        modules: [
-          "financialData",
-          "summaryDetail",
-          "summaryProfile",
-          "earnings",
-        ],
-      },
-      function (err, result) {
-        if (err) {
-          var error = "No symbol found";
-          res.json({
-            message: error,
-          });
-          //throw new Error("Ticker does not exist!");
-        } else {
-          //tickerList.push(ticker, result.financialData.currentPrice)
-          //console.log(result.financialData);
-          res.json({
-            message: `${req.params.symbol} was found!`,
-            symbol: `${req.params.symbol}`,
-            financialData: result.financialData,
-            summaryDetail: result.summaryDetail,
-            summaryProfile: result.summaryProfile,
-            earnings: result.earnings,
-          });
-          //resolve(result.financialData)
-        }
-      }
-    );
-  });
+exports.getTickerFinancialData = async (req, res) => {
+  try {
+    const symbol = req.params.symbol;
+    const queryOptions = {
+      modules: ["financialData", "summaryDetail", "summaryProfile", "earnings"],
+    };
+
+    const result = await yahooFinance.quoteSummary(symbol, queryOptions);
+
+    res.json({
+      message: `${symbol} was found!`,
+      symbol: symbol,
+      financialData: result.financialData,
+      summaryDetail: result.summaryDetail,
+      summaryProfile: result.summaryProfile,
+      earnings: result.earnings,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "No symbol found",
+      error: error.message,
+    });
+  }
 };
 
-exports.getTickerChartData = (req, res) => {
-  return new Promise(function (resolve, reject) {
-    yahooFinance.historical(
-      {
-        symbol: req.query.symbol,
-        from: req.query.start,
-        to: req.query.end,
-        period: req.query.period,
-      },
-      function (err, quotes) {
-        if (err) {
-          var error = "No symbol found";
+exports.getTickerChartData = async (req, res) => {
+  try {
+    const query = req.query.symbol;
+    const queryOptions = {
+      period1: req.query.start,
+      period2: req.query.end,
+      interval: req.query.period,
+    };
 
-          res.json({
-            message: error,
-          });
-        } else {
-          res.json({
-            message: `${req.query.symbol} data was found!`,
-            symbol: `${req.query.symbol}`,
-            data: quotes,
-          });
-        }
-      }
-    );
-  });
+    const result = await yahooFinance.chart(query, queryOptions);
+
+    res.json({
+      message: `${query} data was found!`,
+      symbol: query,
+      data: result,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Error fetching data",
+      error: error.message,
+    });
+  }
 };
